f645377eb6312eaed19f478cb1d6300d86136a06
DereKk8 <130619756+DereKk8@users.noreply.github.com>
2024-08-05 12:05:24 -0500

    Update ejEstructuras.cpp

---
+++ ejEstructuras.cpp
@@ -0,0 +1,21 @@
+void invertirCadena (string cadena){
+    std::queue<string> pila;
+    std::string palabra;
+    
+    for(int i = 0; i < cadena.lenght(); i++){
+        string letra = cadena.subsrt(i)
+        if(letra != " "){
+             palabra += letra
+         }else{
+             pila.push(palabra);
+             palabra = "";
+         }   
+    }
+
+    while(!pila.Empty()){
+       string salida = pila.top()
+       pila.pop();
+       std:: cout << salida << " ";
+    }
+
+}
